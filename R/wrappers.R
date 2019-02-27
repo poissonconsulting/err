4d1cd95b4a2d9c28a386824858c710a63de4b302
Joe Thorley <joe@poissonconsulting.ca>
2019-02-27 10:28:38 -0800

    go with default brackets with wrappers

--- R/wrappers.R
+++ R/wrappers.R
@@ -14,25 +14,25 @@ NULL
 #' @describeIn err_wrappers Concatenation with Commas and And Conjunction
 #' @export
 cc_and <- function(object) {
-  cc(object, conjunction = "and", bracket = "'")
+  cc(object, conjunction = "and")
 }
 
 #' @describeIn err_wrappers Concatenation with Commas and Or Conjunction
 #' @export
 cc_or <- function(object) {
-  cc(object, conjunction = "or", bracket = "'")
+  cc(object, conjunction = "or")
 }
 
 #' @describeIn err_wrappers Customizable Object Aware String and And Conjunction
 #' @export
 co_and <- function(object, one = "%o has %n value%s: %c", object_name = substitute(object)) {
   object_name <- err_deparse(object_name)
-  co(object, one = one, conjunction = "and", bracket = "'", object_name = object_name)
+  co(object, one = one, conjunction = "and", object_name = object_name)
 }
 
 #' @describeIn err_wrappers Customizable Object Aware String and Or Conjunction
 #' @export
 co_or <- function(object, one = "%o has %n value%s: %c", object_name = substitute(object)) {
   object_name <- err_deparse(object_name)
-  co(object, one = one, conjunction = "or", bracket = "'", object_name = object_name)
+  co(object, one = one, conjunction = "or", object_name = object_name)
 }
